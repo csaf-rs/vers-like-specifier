2fb008ee409a782e855c3b82b6b77529e3561348
Johannes Pein <johannes.pein@xitaso.com>
2026-05-11 17:57:50 +0200

    docs: improve links

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,19 @@
 //! of a [vers](https://github.com/package-url/vers-spec) URL *without* the
 //! `vers:<scheme>/` prefix.
 //!
-//!
 //! | Type | Description |
 //! |------|-------------|
-//! | [`Vls`] | Either a list of VersionConstraints or a single wildcard |
-//! | [`VersionConstraint`] | A Comparator / VersionString  pair |
-//! | [`Comparator`] | A comparator used by VersionConstraint |
-//! | [`VersionString`] | A validated version string, used by VersionConstraint |
+//! | [`Vls`] | Either at least one [`VersionConstraint`] or a single wildcard |
+//! | [`VersionConstraint`] | A [`Comparator`] / [`VersionString`] pair |
+//! | [`Comparator`] | A comparator used by [`VersionConstraint`] |
+//! | [`VersionString`] | A validated version string, used by [`VersionConstraint`] |
 //!
 //! ## Error Types
 //!
 //! | Type | Description |
 //! |------|-------------|
 //! | [`VlsError`] | Returned when parsing a vls string fails |
-//! | [`VersionConstraintError`] | Returned when parsing a single VersionConstraint fails |
+//! | [`VersionConstraintError`] | Returned when parsing a single [`VersionConstraint`] fails |
 
 pub use constraint::{Comparator, VersionConstraint, VersionConstraintError, VersionString};
 pub use vls::{Vls, VlsError};
